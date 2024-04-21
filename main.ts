6fd5a0def840c6090aece78d01be00662210d8e4
mrgarth-teacher <110578347+mrgarth-teacher@users.noreply.github.com>
2024-04-21 11:14:45 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showString("Hi!")
+basic.showString("I'm Mr Garth")
+basic.showString("I")
+basic.clearScreen()
+basic.showIcon(IconNames.Heart)
+basic.clearScreen()
+basic.showString("Digitech")
+basic.forever(function () {
+	
+})
